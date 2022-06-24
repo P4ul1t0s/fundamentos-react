3bf4c5bfe6c5faee03075a00f2790f757cc9e056
Paulo <paulomafra2004@gmail.com>
2022-06-24 09:55:15 -0300

    Primeiro Componente II

--- src/components/basics/Primeiro.js
+++ src/components/basics/Primeiro.js
@@ -1,3 +1,11 @@
-export default function Primeiro(){ //*Nome de function é opcional
-    return 'Primeiro Componente'
+import React from "react"
+
+export default function Primeiro(){ //*Nome da function (Primeiro) é opcional
+    const msg = 'Seja bem vindo(a)!'
+    return(
+        <div>
+            <h2>Primeiro Componente</h2>
+            <p>{msg}</p>
+        </div>
+    )
 }
