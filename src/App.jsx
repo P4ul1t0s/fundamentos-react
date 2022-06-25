5ff9bd1ed3a42a2179ee6386f4965bd9dbe10873
Paulo <paulomafra2004@gmail.com>
2022-06-25 02:05:43 -0300

    Repetição

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,17 @@ import Random from './components/basics/Random'
 import Card from './components/layout/Card'
 import Famila from './components/basics/Famila'
 import FamiliaMembro from './components/basics/FamiliaMembro'
+import ListaAlunos from './components/loop/ListaAlunos'
 import './index.css'
 
 export default () => (
     <div className="App">
         <h1>Fundamentos do React</h1>
         <div className="Cards">
+            <Card titulo="#06 - Repetição" color="#FA7C65">
+                <ListaAlunos/>
+            </Card>
+
             <Card titulo="#05 - Componente com Filhos" color="#00C8F8">
                 <Famila sobrenome="Silva">
                     <FamiliaMembro nome="Gustavo"/>  
---
+++ src/components/loop/ListaAlunos.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import alunos from '../../data/alunos'
+
+export default props => {
+    const lis = alunos.map((aluno) => {
+        return(
+            <li key={alunos.id}>
+                {aluno.id} - {aluno.nome} ➡ {aluno.nota}
+            </li>
+        )
+    })
+
+    return(
+        <div>
+            <ul>
+                {lis}
+            </ul>
+        </div>
+    )
+}
